0a245718d29e07f550f1682d8c46e13a4a792572
GayathriL <l.gayathri96@gmail.com>
2017-12-07 09:38:43 +0530

    api call

--- create.php
+++ create.php
@@ -6,29 +6,33 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
  
  $data = json_decode(file_get_contents("php://input"), true); 
-require_once("cruddb.php");
-$create=new DBController();
+   require_once("cruddb.php");
+   $create=new DBController();
+    $firstname=$_POST['fname'];
+     $lastname=$_POST['lname'];
+     $qualification=$_POST['qualification'];
+     $city=$_POST['city'];
+     $state=$_POST['state'];
 
-if($_SERVER['REQUEST_METHOD'] == "POST") {
-$firstname=$_POST['fname'];
-$lastname=$_POST['lname'];
-$qualification=$_POST['qualification'];
-$city=$_POST['city'];
-$state=$_POST['state'];
+   if($_SERVER['REQUEST_METHOD'] == "POST") {
+    $query="INSERT INTO details(firstname,lastname,qualification,city,state) VALUES('$firstname','$lastname','$qualification','$city','$state') ";
+    $results=$create->runQuery($query);
 
-$query="INSERT INTO details(firstname,lastname,qualification,city,state) VALUES('$firstname','$lastname','$qualification','$city','$state') ";
-$results=$create->runQuery($query);
+ 
 
+echo json_encode($results);
 
 }
-if($results)
-{
-	echo "inserted successfully";
-}
-else
-{
-	echo "insertion not happening";
-}
+// if($results)
+//  {
+//  	echo "inserted successfully";
+//  }
+//  else
+//  {
+//  	echo "insertion not happening";
+// }
+
+
 
 
 ?>
